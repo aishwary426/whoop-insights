c067ef56d7394faaaf8c3a326c35d2486eba2483
Aishwary Singh <aishwary@Aishwarys-MacBook-Air.local>
2025-11-22 14:47:02 +0530

    Enable standalone output for Docker build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
+  output: 'standalone',
   experimental: {
     // serverActions is true by default in Next.js 14
   },
